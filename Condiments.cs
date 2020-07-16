819e0fcc3729fce9e3fa09e1d6ec35ef53aac4e9
oasispremium <alex@alexandrelepage.net>
2020-07-16 14:12:22 -0400

    Init push
    Initial version of FFTM

---
+++ Condiments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    abstract class Condiments
+    {
+        //We know condiments have some nutritional values but we can't specify exactly how much
+        //And we know that each condiments NEED to have those
+        //Abstract let us do that
+        abstract public int GetCalories();
+        public abstract int GetGramsOfSugar();
+        public abstract int GetGramsOfProtein();
+    }
+}
